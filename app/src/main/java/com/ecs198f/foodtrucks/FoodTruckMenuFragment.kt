683b19f05abffdc29c56b94a0dace450acae91bb
hodangkhoinguyen <50374959+hodangkhoinguyen@users.noreply.github.com>
2022-05-17 10:09:32 -0700

    add itemDao to MenuFragment

--- app/src/main/java/com/ecs198f/foodtrucks/FoodTruckMenuFragment.kt
+++ app/src/main/java/com/ecs198f/foodtrucks/FoodTruckMenuFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.ecs198f.foodtrucks.databinding.FragmentFoodTruckMenuBinding
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -30,17 +32,24 @@ class FoodTruckMenuFragment(private val foodTruck: FoodTruck) : Fragment() {
 
         (requireActivity() as MainActivity).apply {
             title = foodTruck.name
-
+            var itemDao = db.itemDao()
             foodTruckService.listFoodItems(foodTruck.id).enqueue(object : Callback<List<FoodItem>> {
                 override fun onResponse(
                     call: Call<List<FoodItem>>,
                     response: Response<List<FoodItem>>
                 ) {
-                    recyclerViewAdapter.updateItems(response.body()!!)
+                    val body = response.body()
+                    recyclerViewAdapter.updateItems(body!!)
+                    lifecycleScope.launch {
+                        itemDao.removeItem(foodTruck.id)
+                        itemDao.addItem(body!!)
+                    }
                 }
 
                 override fun onFailure(call: Call<List<FoodItem>>, t: Throwable) {
-                    //throw t
+                    lifecycleScope.launch {
+                        recyclerViewAdapter.updateItems(itemDao.listItemsOfTruck(foodTruck.id))
+                    }
                 }
             })
         }
